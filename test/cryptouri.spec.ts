3fe57f8cf57ce05bb466afd7cf01afe3d897f5ee
Tony Arcieri <bascule@gmail.com>
2019-10-07 07:27:15 -0700

    README.md: Update CryptoURI examples with root namespace changes
    See https://github.com/cryptouri/cryptouri-spec/issues/2

--- test/cryptouri.spec.ts
+++ test/cryptouri.spec.ts
@@ -4,7 +4,7 @@ import CryptoURI from "../src/cryptouri";
 
 @suite class CryptoURISpec {
   @test "parses CryptoURIs"() {
-    const exampleURI = "crypto:public:key:ed25519:6adfsqvzky9t042tlmfujeq88g8wzuhnm2nzxfd0qgdx3ac82ydq3pkr2c";
+    const exampleURI = "crypto:pub:key:ed25519:6adfsqvzky9t042tlmfujeq88g8wzuhnm2nzxfd0qgdx3ac82ydqf03cvv";
     expect(CryptoURI.parse(exampleURI)).to.eql(exampleURI);
   }
 }
